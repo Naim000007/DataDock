665c9249e45c2132badf264d04e3daabfa7924b3
Naim000007 <mdnaim01303202218@gmail.com>
2024-11-10 12:15:01 +0600

    add global css

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex h-screen items-center justify-center bg-amber-600 ">
       <h1 className="text-3xl">The only solution you need</h1>
-    </>
+    </div>
   );
 }
